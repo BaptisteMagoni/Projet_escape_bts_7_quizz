b3748f4e30dd38c02e4d2d079e6d95303eb01a45
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-06 12:10:43 +0100

    Update Programmation/Application/Escape_Quiz_Projet_BTS/mainwindow.cpp

--- Programmation/Application/Escape_Quiz_Projet_BTS/mainwindow.cpp
+++ Programmation/Application/Escape_Quiz_Projet_BTS/mainwindow.cpp
@@ -36,6 +36,28 @@ void MainWindow::CheckButton(){
     QString textCurrent = ui->comboBox_com->currentText();
     QStringList listContent = textCurrent.split(":");
     QString comPort = listContent.at(0);
+    comPort = comPort.replace(" ", "");
     QString nameDevice = listContent.at(1);
+    foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts())
+            if(info.portName().compare(comPort) == 0)
+                openPort(comPort, QSerialPort::Baud9600);
 
+    m_serial->write("OPEN");
+}
+
+bool MainWindow::openPort(QString portName, QSerialPort::BaudRate actualBaudRate)
+{
+    bool open = false;
+
+    if (NULL != m_serial)
+    {
+        open = m_serial->open(QIODevice::WriteOnly);
+        m_serial->setPortName(portName);
+        m_serial->setBaudRate(actualBaudRate);
+        m_serial->setDataBits(QSerialPort::Data8);
+        m_serial->setParity(QSerialPort::NoParity);
+        m_serial->setStopBits(QSerialPort::OneStop);
+        m_serial->setFlowControl(QSerialPort::HardwareControl);
+    }
+    return open;
 }
