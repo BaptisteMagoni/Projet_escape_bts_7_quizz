5cf9e2c24a8e79f973898f6071c7e92b67237c88
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-06 11:34:55 +0100

    Update Programmation/Application/Escape_Quiz_Projet_BTS/mainwindow.cpp

---
+++ Programmation/Application/Escape_Quiz_Projet_BTS/mainwindow.cpp
@@ -0,0 +1,41 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <iostream>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+
+using namespace std;
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    m_serial(new QSerialPort(this))
+{
+    ui->setupUi(this);
+    setComPort();
+    setObject();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::setComPort(){
+    foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts())
+            ui->comboBox_com->addItem(info.portName() + " : " + info.description());
+}
+
+void MainWindow::setObject(){
+    connect(ui->button_start, SIGNAL (released()), this, SLOT(CheckButton()));
+}
+
+void MainWindow::CheckButton(){
+    cout << ui->comboBox_com->currentText().toStdString() << endl;
+    QString textCurrent = ui->comboBox_com->currentText();
+    QStringList listContent = textCurrent.split(":");
+    QString comPort = listContent.at(0);
+    QString nameDevice = listContent.at(1);
+    cout << sizeof(comPort);
+}
