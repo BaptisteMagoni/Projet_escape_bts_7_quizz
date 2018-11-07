527722bc96bd615c538321ca893034815fcccc43
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-07 23:20:46 +0100

    Update Programmation/Application/Escape_Quiz_Projet_BTS/mainwindow.cpp

--- Programmation/Application/Escape_Quiz_Projet_BTS/mainwindow.cpp
+++ Programmation/Application/Escape_Quiz_Projet_BTS/mainwindow.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
 #include <iostream>
@@ -9,8 +10,7 @@ using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow),
-    m_serial(new QSerialPort(this))
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     setComPort();
@@ -20,11 +20,12 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    m_serial->close();
 }
 
 void MainWindow::setComPort(){
     foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts())
-            ui->comboBox_com->addItem(info.portName() + " : " + info.description());
+        ui->comboBox_com->addItem(info.portName() + " : " + info.description());
 }
 
 void MainWindow::setObject(){
@@ -37,27 +38,35 @@ void MainWindow::CheckButton(){
     QStringList listContent = textCurrent.split(":");
     QString comPort = listContent.at(0);
     comPort = comPort.replace(" ", "");
-    QString nameDevice = listContent.at(1);
     foreach (const QSerialPortInfo &info, QSerialPortInfo::availablePorts())
-            if(info.portName().compare(comPort) == 0)
-                openPort(comPort, QSerialPort::Baud9600);
-
-    m_serial->write("OPEN");
+        if(info.portName().compare(comPort) == 0)
+            openPort(comPort, QSerialPort::Baud9600);
+    sendData("ALLUME");
 }
 
-bool MainWindow::openPort(QString portName, QSerialPort::BaudRate actualBaudRate)
+void MainWindow::openPort(QString portName, QSerialPort::BaudRate actualBaudRate)
 {
-    bool open = false;
-
-    if (NULL != m_serial)
-    {
-        open = m_serial->open(QIODevice::WriteOnly);
-        m_serial->setPortName(portName);
+    m_serial = new QSerialPort(this);
+    m_serial->setPortName(portName);
+    if(m_serial->open(QSerialPort::ReadWrite)){
         m_serial->setBaudRate(actualBaudRate);
         m_serial->setDataBits(QSerialPort::Data8);
         m_serial->setParity(QSerialPort::NoParity);
         m_serial->setStopBits(QSerialPort::OneStop);
-        m_serial->setFlowControl(QSerialPort::HardwareControl);
+        m_serial->setFlowControl(QSerialPort::NoFlowControl);
+        m_serial->open(QIODevice::ReadWrite);
+        cout << "Conexion" << endl;
+    }else{
+        cout << "Error code : " << m_serial->error() << endl;
+        cout << "Error string : " << m_serial->errorString().toStdString() << endl;
     }
-    return open;
+}
+
+void MainWindow::sendData(const char * data){
+    m_serial->write(data);
+    cout << "Envoi : " << data << endl;
+}
+
+char MainWindow::readData(){
+
 }
