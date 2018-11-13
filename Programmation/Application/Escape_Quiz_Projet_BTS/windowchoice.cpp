ddebbfea7f36d7a94b77830db7f719899d688370
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-13 22:47:09 +0100

    Update Programmation/Application/Escape_Quiz_Projet_BTS/windowchoice.cpp

--- Programmation/Application/Escape_Quiz_Projet_BTS/windowchoice.cpp
+++ Programmation/Application/Escape_Quiz_Projet_BTS/windowchoice.cpp
@@ -2,15 +2,38 @@
 #include "ui_windowchoice.h"
 #include <QSerialPort>
 #include <QSerialPortInfo>
+#include <windowquestion.h>
+#include <iostream>
 
-WindowChoice::WindowChoice(QWidget *parent, QSerialPort *m_serial) :
+using namespace std;
+
+WindowChoice::WindowChoice(QWidget *parent, QSerialPort *serial) :
     QWidget(parent),
-    wc(new Ui::Form)
+    wc(new Ui::ApplicationMode)
 {
+    m_serial = serial;
     wc->setupUi(this);
+    m_parent = parent;
+    init_widget_question();
 }
 
 WindowChoice::~WindowChoice()
 {
     delete wc;
+}
+
+void WindowChoice::init_widget_question(){
+    connect(wc->pushButton_musicale, SIGNAL (released()), this, SLOT(event_button_choice()));
+    connect(wc->pushButton_informatique, SIGNAL (released()), this, SLOT(event_button_choice()));
+}
+
+void WindowChoice::link_widget_question(){
+   WindowQuestion *question = new WindowQuestion(m_parent, m_serial);
+   question->show();
+}
+
+void WindowChoice::event_button_choice(){
+    this->close();
+    link_widget_question();
+    cout << "Caca" << endl;
 }
