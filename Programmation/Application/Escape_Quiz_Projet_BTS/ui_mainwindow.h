c63b0bdd1f25c9dd8befdb39d08ea8db0036e48e
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-13 22:47:16 +0100

    Update Programmation/Application/Escape_Quiz_Projet_BTS/ui_mainwindow.h

--- Programmation/Application/Escape_Quiz_Projet_BTS/ui_mainwindow.h
+++ Programmation/Application/Escape_Quiz_Projet_BTS/ui_mainwindow.h
@@ -50,7 +50,7 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(484, 613);
+        MainWindow->resize(484, 580);
         ApplicationGlobale = new QWidget(MainWindow);
         ApplicationGlobale->setObjectName(QStringLiteral("ApplicationGlobale"));
         verticalLayout_2 = new QVBoxLayout(ApplicationGlobale);
