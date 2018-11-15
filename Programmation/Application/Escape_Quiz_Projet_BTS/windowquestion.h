eef09b3d6a371037674fb2d90c40f970fb48d246
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-15 16:23:21 +0100

    Update windowquestion.h

--- Programmation/Application/Escape_Quiz_Projet_BTS/windowquestion.h
+++ Programmation/Application/Escape_Quiz_Projet_BTS/windowquestion.h
@@ -4,6 +4,10 @@
 #include <QWidget>
 #include <QSerialPort>
 #include <QSerialPortInfo>
+#include <QFile>
+#include <vector>
+#include <iostream>
+#include <QString>
 
 namespace Ui{
     class ApplicationQuestion;
@@ -15,15 +19,20 @@ class WindowQuestion : public QWidget
     Q_OBJECT
 
 public:
-    WindowQuestion(QWidget *parent, QSerialPort *serial);
+    WindowQuestion(QWidget *parent, QSerialPort *serial, QString type_question);
     ~WindowQuestion();
 
 private:
     Ui::ApplicationQuestion *wq;
     QSerialPort *m_serial;
+    QString m_type_question;
+    std::vector<QString> question;
+    int question_number;
 
 private:
-    void read_question();
+    void read_one_question();
+    void open_file();
+    void display_question();
 };
 
 #endif // WINDOWQUESTION_H
