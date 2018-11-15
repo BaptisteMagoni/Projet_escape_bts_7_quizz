22923afd2ec4e495c1c9974f6d154840b0eb7626
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-15 19:32:00 +0100

    Update windowquestion.h

--- Programmation/Application/Escape_Quiz_Projet_BTS/windowquestion.h
+++ Programmation/Application/Escape_Quiz_Projet_BTS/windowquestion.h
@@ -26,13 +26,25 @@ private:
     Ui::ApplicationQuestion *wq;
     QSerialPort *m_serial;
     QString m_type_question;
-    std::vector<QString> question;
+    QString answer;
+    std::vector<QString> m_question;
+    std::vector<QString> sequence_question;
+    std::vector<bool> answer_player;
     int question_number;
+    QWidget *m_parent;
+
 
 private:
     void read_one_question();
     void open_file();
-    void display_question();
+    void change_page();
+    void init_button_event();
+    void check_answer(QString answerplayer);
+    int get_nb_question();
+    QString int_to_str(int num);
+
+private slots:
+    void event_button();
 };
 
 #endif // WINDOWQUESTION_H
