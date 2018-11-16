b00ff992a81252da3ea478e55e07f7f76eecbbe7
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-16 10:27:49 +0100

    Update Programmation/Application/Escape_Quiz_Projet_BTS/windowend.h

--- Programmation/Application/Escape_Quiz_Projet_BTS/windowend.h
+++ Programmation/Application/Escape_Quiz_Projet_BTS/windowend.h
@@ -15,11 +15,16 @@ class WindowEnd : public QWidget
     Q_OBJECT
 
 public:
-    WindowEnd(QWidget *parent, QSerialPort *serial, std::vector<bool> score);
+    WindowEnd(QWidget *parent, QSerialPort *serial, std::vector<bool> score, QString answer_number_all);
     ~WindowEnd();
 
 private:
     Ui::ApplicationEnd *we;
+    std::vector<bool> m_score;
+    QString m_number_all;
+
+private:
+    int count_score();
 };
 
 #endif // WINDOWEND_H
