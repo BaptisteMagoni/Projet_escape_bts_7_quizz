6e68ca2fb8ea711e0ddf0b05f15131db356a3ac7
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-16 10:27:45 +0100

    Update Programmation/Application/Escape_Quiz_Projet_BTS/windowend.cpp

--- Programmation/Application/Escape_Quiz_Projet_BTS/windowend.cpp
+++ Programmation/Application/Escape_Quiz_Projet_BTS/windowend.cpp
@@ -5,13 +5,24 @@
 #include <QSerialPort>
 #include <iostream>
 
-WindowEnd::WindowEnd(QWidget *parent, QSerialPort *serial, std::vector<bool> score) :
+WindowEnd::WindowEnd(QWidget *parent, QSerialPort *serial, std::vector<bool> score, QString answer_number_all) :
     QWidget(parent),
     we(new Ui::ApplicationEnd)
 {
     we->setupUi(this);
+    m_score = score;
+    m_number_all = answer_number_all;
+    we->label_score->setText("Vous avez un total de " + QString::number(count_score()) + "/" + m_number_all + " bonne réponse !");
 }
 
 WindowEnd::~WindowEnd(){
+    delete we;
+}
 
+int WindowEnd::count_score(){
+    int nb = 0;
+    for(int i=0;i<m_score.size();i++)
+        if(m_score.at(i))
+            nb++;
+    return nb;
 }
