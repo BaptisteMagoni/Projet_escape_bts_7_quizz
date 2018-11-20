cd06afb83e195f600a20199f7ac93702e677aa14
BaptisteMagoni <34521668+BaptisteMagoni@users.noreply.github.com>
2018-11-20 11:49:18 +0100

    Rapport

---
+++ Programmation/Application/Escape_Quiz_Projet_BTS/client.cpp
@@ -0,0 +1,43 @@
+#include "client.h"
+#include <QtNetwork>
+#include <stdlib.h>
+#include <iostream>
+#include <QTcpSocket>
+#include <QNetworkSession>
+#include <QByteArray>
+
+Client::Client() : m_networkSession(0)
+{
+
+    std::cout << "Application Client" << std::endl;
+        m_tcpSocket = new QTcpSocket(this);
+        QNetworkConfiguration config;
+
+        std::cout << "Ouverture session" << std::endl;
+        m_networkSession = new QNetworkSession(config, this);
+
+        m_networkSession->open();
+
+        m_blockSize = 0;
+        m_tcpSocket->abort();
+
+        // connexion au serveur sur le port 53000
+        m_tcpSocket->connectToHost( QHostAddress("10.16.3.214").toString(),53000);
+        send_data("GAGNE:7");
+
+}
+
+void Client::send_data( const std::string& s )
+{
+    std::cout << "Envoi de : " << s << std::endl;
+    QString texte = tr(s.c_str());
+    QByteArray block;
+    QDataStream out(&block, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_4_0);
+    out << (quint16)0;
+    out << texte;
+    out.device()->seek(0);
+    out << (quint16)(block.size() - sizeof(quint16));
+
+    m_tcpSocket->write(block);
+}
---
+++ Programmation/Application/Escape_Quiz_Projet_BTS/client.h
@@ -0,0 +1,19 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QTcpSocket>
+#include <QNetworkSession>
+
+class Client
+{
+public:
+    Client();
+
+private:
+    QTcpSocket *m_tcpSocket;
+    quint16 m_blockSize;
+    QNetworkSession *m_networkSession;
+    void send_data( const std::string& s);
+};
+
+#endif // CLIENT_H
